0f5eeac852bd1b2ed4209f3e0367a4b807f16912
Isaac Bustamante <izaack.97@gmail.com>
2020-05-26 12:54:03 -0700

    Se implemento un alert y se cambia el titulo del header con un prompt

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  titulo: string;
+
+  constructor(private alertCtrl: AlertController) { }
 
   ngOnInit() {
+  }
+  // Prensemta el alert
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Cancelar');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('Boton Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  // Presenta aler con prompt
+  async presentAlertPrompt() {
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Prompt!',
+      inputs: [
+        {
+          name: 'txtTitle',
+          type: 'text',
+          placeholder: 'Introduzca el titulo'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (data) => {
+            this.titulo = data.txtTitle;
+            console.log('Confirm Ok', data);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
